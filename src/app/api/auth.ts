d58296cc9b6761651fd50d29f25a8a84a159dfc1
estanip <estani.pettigrew@gmail.com>
2023-09-17 19:08:06 -0300

    Add Register Flow

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from "@reduxjs/toolkit";
 import { loginError, loginPending, loginSuccess } from "../store/modules/Auth";
 import { LoginDispatchI } from "./interfaces/Auth";
+import { RegisterFormI } from "../interfaces/auth/Auth";
 
 export const signIn =
   (email: string, password: string) =>
@@ -13,6 +14,28 @@ export const signIn =
           email,
           password,
         }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          if (response?.success) dispatch(loginSuccess(response));
+          else if (!response?.success) dispatch(loginError(response));
+        });
+    } catch (error: any) {
+      dispatch(loginError(error));
+    }
+  };
+
+export const signUp =
+  (user: RegisterFormI) =>
+  async (dispatch: Dispatch<LoginDispatchI>): Promise<void> => {
+    dispatch(loginPending());
+    try {
+      await fetch("http://192.168.1.6:80/auth/register", {
+        method: "POST",
+        body: JSON.stringify(user),
         headers: {
           "Content-Type": "application/json",
         },
