c281778d2603d5ec0bb47437d234a2392f8c2670
estanip <estani.pettigrew@gmail.com>
2024-02-02 10:28:50 -0300

    add AsyncStorage

--- src/app/shared/storage/index.ts
+++ src/app/shared/storage/index.ts
@@ -1,27 +1,26 @@
-import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const storage = new Storage({
-    // maximum capacity, default 1000 key-ids
-    size: 1000,
-
-    // Use AsyncStorage for RN apps, or window.localStorage for web apps.
-    // If storageBackend is not set, data will be lost after reload.
-    storageBackend: AsyncStorage, // for web: window.localStorage
-
-    // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
-    // can be null, which means never expire.
-    defaultExpires: 1000 * 3600 * 24,
-
-    // cache data in the memory. default is true.
-    enableCache: true,
-
-    // if data was not found in storage or expired data was found,
-    // the corresponding sync method will be invoked returning
-    // the latest data.
-    sync: {
-        // we'll talk about the details later.
-    },
-});
-
-export default storage;
+import { Platform } from 'react-native';
+class SupabaseStorage {
+    async getItem(key: string) {
+      if (Platform.OS === "web") {
+        if (typeof localStorage === "undefined") {
+          return null;
+        }
+        return localStorage.getItem(key);
+      }
+      return AsyncStorage.getItem(key);
+    }
+    async removeItem(key: string) {
+      if (Platform.OS === "web") {
+        return localStorage.removeItem(key);
+      }
+      return AsyncStorage.removeItem(key);
+    }
+    async setItem(key: string, value: string) {
+      if (Platform.OS === "web") {
+        return localStorage.setItem(key, value);
+      }
+      return AsyncStorage.setItem(key, value);
+    }
+  }
+  
