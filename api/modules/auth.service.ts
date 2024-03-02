10838c75f6c50f4382a6590e7840abe48f4282f2
estanip <estani.pettigrew@gmail.com>
2024-03-02 12:28:26 -0300

    refactor: improve types & responses

--- api/modules/auth.service.ts
+++ api/modules/auth.service.ts
@@ -1,25 +1,36 @@
+import {
+    ILoginRequest,
+    ILoginSuccessResponse,
+    IRegisterRequest,
+    IRegisterResponse,
+} from '../interfaces/auth.interfaces';
+
 import { apiPost, apiUris } from '@/constants/api.constants';
 import { environment } from '@/environments/environment';
-import { IServerResponse } from '../interfaces';
-import { IUserRequest, LoginResponse } from '../interfaces/auth.interface';
 
-export const login = async (
-    email: string,
-    password: string,
-): Promise<LoginResponse | undefined> => {
+export const login = async (request: ILoginRequest): Promise<ILoginSuccessResponse | undefined> => {
     try {
-        return await apiPost(`${environment.SERVER_URI}${apiUris.login}`, {
-            email,
-            password,
-        });
+        const response = await apiPost(`${environment.SERVER_URI}${apiUris.login}`, request);
+        if (response)
+            return {
+                data: response?.data,
+                success: response?.success,
+            };
     } catch (error: unknown) {
         console.log(error);
     }
 };
 
-export const register = async (user: IUserRequest): Promise<IServerResponse | undefined> => {
+export const register = async (
+    request: IRegisterRequest,
+): Promise<IRegisterResponse | undefined> => {
     try {
-        return await apiPost(`${environment.SERVER_URI}${apiUris.register}`, user);
+        const response = await apiPost(`${environment.SERVER_URI}${apiUris.register}`, request);
+        if (response)
+            return {
+                message: response?.message,
+                success: response?.success,
+            };
     } catch (error: unknown) {
         console.log(error);
     }
