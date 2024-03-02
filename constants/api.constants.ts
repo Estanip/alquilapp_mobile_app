4e072a082fc5b0af0a2b94344878c3334e6f09cc
estanip <estani.pettigrew@gmail.com>
2024-03-02 12:25:44 -0300

    refactor: improve api calls

--- constants/api.constants.ts
+++ constants/api.constants.ts
@@ -1,13 +1,32 @@
+import { IServerResponse } from '@/api/interfaces';
+import { TToken } from '@/api/interfaces/auth.interfaces';
+
 export const apiUris = {
     register: '/auth/register',
     login: '/auth/login',
+    courts: '/court',
+    bookings: '/reservation',
+    user: '/user',
 };
 
-export const apiPost = (url: string, body: object) => {
+export const apiGet = (url: string, token: TToken): Promise<IServerResponse> => {
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res) => res.json())
+        .then((response) => response);
+};
+
+export const apiPost = (url: string, body: object, token?: TToken): Promise<IServerResponse> => {
     return fetch(url, {
         method: 'POST',
         body: JSON.stringify(body),
         headers: {
+            authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
     })
