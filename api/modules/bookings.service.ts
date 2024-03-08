6db0ba05bc9f0b8a0cfe713909e4c56aab32f5f5
estanip <estani.pettigrew@gmail.com>
2024-03-08 15:19:04 -0300

    feat: add edit service

--- api/modules/bookings.service.ts
+++ api/modules/bookings.service.ts
@@ -1,11 +1,48 @@
 import { IServerResponse } from '../interfaces';
 import { TToken } from '../interfaces/auth.interfaces';
+import { IBookRequest } from '../interfaces/book.interfaces';
 import { TReservations } from '../interfaces/booking.interfaces';
 
-import { apiDelete, apiGet, apiUris } from '@/constants/api.constants';
+import { apiDelete, apiGet, apiPut, apiUris } from '@/constants/api.constants';
 import { environment } from '@/environments/environment';
 
 export function BookingsService() {
+    const cancel = async (
+        token: TToken,
+        booking_id: string,
+    ): Promise<Pick<IServerResponse, 'statusCode'> | void> => {
+        try {
+            const response = await apiDelete(
+                `${environment.SERVER_URI}${apiUris.reservation}/${booking_id}`,
+                token,
+            );
+            return response;
+        } catch (error: unknown) {
+            console.log(error);
+        }
+    };
+
+    const edit = async (
+        token: TToken,
+        request: IBookRequest,
+        booking_id: string,
+    ): Promise<Pick<IServerResponse, 'message' | 'success'> | undefined> => {
+        try {
+            const response = await apiPut(
+                `${environment.SERVER_URI}${apiUris.reservation}/${booking_id}`,
+                request,
+                token,
+            );
+            if (response)
+                return {
+                    message: response?.message,
+                    success: response?.success,
+                };
+        } catch (error: unknown) {
+            console.log(error);
+        }
+    };
+
     const getByOwner = async (
         token: TToken,
         user_id: string,
@@ -21,23 +58,9 @@ export function BookingsService() {
         }
     };
 
-    const cancelById = async (
-        token: TToken,
-        booking_id: string,
-    ): Promise<Pick<IServerResponse, 'statusCode'> | void> => {
-        try {
-            const response = await apiDelete(
-                `${environment.SERVER_URI}${apiUris.reservation}/${booking_id}`,
-                token,
-            );
-            return response;
-        } catch (error: unknown) {
-            console.log(error);
-        }
-    };
-
     return {
+        cancel,
+        edit,
         getByOwner,
-        cancelById,
     };
 }
