ca6009d816f0f55f64e5f46559e29e590a4dc0a9
estanip <estani.pettigrew@gmail.com>
2024-03-10 17:08:58 -0300

    refactor: add password_reset to params

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,6 +2,7 @@ import { ParamListBase, RouteProp } from '@react-navigation/native';
 
 export interface INavigationParams {
     _id?: string;
+    _password_reset?: string;
 }
 export interface IRoute extends RouteProp<ParamListBase> {
     params?: INavigationParams;
