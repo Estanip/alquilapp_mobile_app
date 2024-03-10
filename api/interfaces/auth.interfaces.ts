d823cfc6b6b7f48127467a17433fc3894ee70499
estanip <estani.pettigrew@gmail.com>
2024-03-10 17:03:58 -0300

    refactor: improve interfaces

--- api/interfaces/auth.interfaces.ts
+++ api/interfaces/auth.interfaces.ts
@@ -7,6 +7,10 @@ export interface ILoginRequest {
     email: string;
     password: string;
 }
+
+export interface IResetPasswordRequest extends Pick<ILoginRequest, 'email'> {
+    new_password: string;
+}
 export interface IRegisterRequest extends IRegisterForm {
     is_enabled?: boolean;
     is_membership_validated?: boolean;
@@ -15,6 +19,9 @@ export interface ILoginSuccessResponse
     extends Omit<IServerResponse, 'data' | 'message' | 'path' | 'statusCode' | 'timestamp'> {
     data: IUserResponse;
 }
+
+export interface IPasswordResetSuccessResponse
+    extends Omit<IServerResponse, 'data' | 'path' | 'statusCode' | 'timestamp'> {}
 export interface IRegisterResponse
     extends Omit<IServerResponse, 'data' | 'path' | 'statusCode' | 'timestamp'> {}
 export type TToken = null | undefined | string;
