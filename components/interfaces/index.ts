752ec4651978863bceb8c0baff9052b48ac6a0a5
estanip <estani.pettigrew@gmail.com>
2024-03-18 13:02:47 -0300

    feat: create Date Picker component

--- components/interfaces/index.ts
+++ components/interfaces/index.ts
@@ -11,3 +11,16 @@ export interface ISelectOptions {
 export interface ITitleProps {
     _title: string;
 }
+
+export interface IDatePickerProps {
+    _setShowDatePicker: () => void;
+    _showDatePicker: boolean;
+    _onCancel: () => void;
+    _date: Date;
+    _onChangeDate: (arg: Date) => void;
+    _formatDate: string;
+    _minimumDate?: Date;
+    _maximumDate?: Date;
+    _hasError?: string;
+    _placeholderText: string;
+}
