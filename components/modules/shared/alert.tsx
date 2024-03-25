47414469b0771c4670715c1d767775d5c2ec5cfd
estanip <estani.pettigrew@gmail.com>
2024-03-25 14:52:40 -0300

    refactor: improve & rename components

--- components/modules/shared/alert.tsx
+++ components/modules/shared/alert.tsx
@@ -2,9 +2,35 @@ import { default as React, ReactNode, createContext, useContext } from 'react';
 import { Text, TextProps, View, ViewProps, ViewStyle } from 'react-native';
 import tailwind, { useAppColorScheme } from 'twrnc';
 
-import { TAlertIconVariants, TAlertVariants } from './interfaces';
-
 import { Icon, IconProps } from '@/components/modules/shared/icon';
+
+import { Style } from 'twrnc';
+
+export type TAlertVariants = {
+    default: Style;
+    success: Style;
+    warning: Style;
+    error: Style;
+};
+
+export type TAlertIconVariants = {
+    default: {
+        light: string | undefined;
+        dark: string | undefined;
+    };
+    error: {
+        light: string | undefined;
+        dark: string | undefined;
+    };
+    success: {
+        light: string | undefined;
+        dark: string | undefined;
+    };
+    warning: {
+        light: string | undefined;
+        dark: string | undefined;
+    };
+};
 
 interface AlertProps extends ViewProps {
     variant?: 'default' | 'success' | 'warning' | 'error';
