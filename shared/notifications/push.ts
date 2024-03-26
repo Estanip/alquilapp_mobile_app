55aed74f810ea4c2e620a6b47b949b4c0cbf2fe0
estanip <estani.pettigrew@gmail.com>
2024-03-26 12:29:13 -0300

    feat: add push notification

---
+++ shared/notifications/push.ts
@@ -0,0 +1,60 @@
+import { NotificationService } from '@/api/modules/notifications.service';
+import Constants from 'expo-constants';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
+
+export async function sendPushNotification(
+    token: string,
+    pushToken: string,
+    title: string,
+    body: string,
+    data: object = {},
+) {
+    await NotificationService().sendPush(token, {
+        to: pushToken,
+        title,
+        body,
+        data,
+    });
+}
+
+export async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('new-emails', {
+            name: 'E-mail notifications',
+            sound: '../../sound.wav',
+            importance: Notifications.AndroidImportance.MAX,
+            lightColor: 'red',
+            vibrationPattern: [0, 250, 250, 250],
+        });
+    }
+
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = await Notifications.getExpoPushTokenAsync({
+            projectId: Constants?.expoConfig?.extra?.eas.projectId,
+        });
+    } else alert('Must use physical device for Push Notifications');
+
+    return token?.data;
+}
