d709cdaa518996320a6025180c45ecdef0a17b15
estanip <estani.pettigrew@gmail.com>
2024-03-26 12:28:23 -0300

    feat: add services to send a push notification

---
+++ api/modules/notifications.service.ts
@@ -0,0 +1,19 @@
+import { apiPost, apiUris } from '@/constants/api.constants';
+import { TToken } from '../interfaces/auth.interfaces';
+
+export function NotificationService() {
+    const sendPush = async (token: TToken, message: object): Promise<void | undefined> => {
+        try {
+            await apiPost(`${apiUris.send_push_notification}`, message, token, {
+                Accept: 'application/json',
+                'Accept-encoding': 'gzip, deflate',
+            });
+        } catch (error: unknown) {
+            console.log(error);
+        }
+    };
+
+    return {
+        sendPush,
+    };
+}
