ed43afe59b1ba696bd4fddb60326bcdf4ac78e60
estanip <estani.pettigrew@gmail.com>
2024-04-15 17:31:09 -0300

    refactorr: prettier

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 declare module '@env' {
-    export const EXPO_PUBLIC_API_PORT: string;
-    export const EXPO_PUBLIC_HOST_LOCAL: string;
-    export const EXPO_PUBLIC_HOST_PROD: string;
-    export const EXPO_PUBLIC_HOST_DEV: string;
-    export const EXPO_PUBLIC_ENVIRONMENT: string;
+  export const EXPO_PUBLIC_API_PORT: string;
+  export const EXPO_PUBLIC_HOST_LOCAL: string;
+  export const EXPO_PUBLIC_HOST_PROD: string;
+  export const EXPO_PUBLIC_HOST_DEV: string;
+  export const EXPO_PUBLIC_ENVIRONMENT: string;
 }
